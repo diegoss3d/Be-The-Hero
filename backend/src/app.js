a7d80ac2e90c73cc49b0cae690f7a5456ceead56
diegoss3d <diegoss3d@gmail.com>
2020-05-28 12:57:35 -0300

    adicionado algumas validacoes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json()) /*Informa para express ir no corpo da requisição e converter o objeto json
+app.use(express.json()); /*Informa para express ir no corpo da requisição e converter o objeto json
 em um objeto javascript*/
 
-app.use(routes)
+app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
 
 /*Executar servidor com node index.js*/
 /* "npm install nodemon -D" para instalar o nodemon, após isso o servidor inicia com "npm start"*/
